f4311d7e5d68d567db70e24ea1b9b37d6729011f
shaolun <shaolun28@gmail.com>
2021-03-09 17:13:50 -0800

    dev

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import CalculatorFormSli11 from "./Components/CalculatorFormSli11/CalculatorFormSli11.js";
 
-function update(){
 
-}
 
 function App() {
 
-  
+  const [equation, setEquation] = useState({});
+
+  function updateEquation(e){
+    setEquation({
+      ...equation,
+      [e.target.id] : e.target.value
+    })
+  }
+
+
+  function doOperation(){
+    switch(equation.operation){
+      case "add":
+        console.log("Result: " + (parseFloat(equation.operand1) + parseFloat(equation.operand2)))
+        break;
+      default:
+        console.log("Sorry we dont have that operation");
+        break;
+    }
+  }
+
+
 
   return (
     <div className="App">
       <h1>Shaolun's Calculator</h1>
-      <CalculatorFormSli11 update={}/>
+      <CalculatorFormSli11 action={doOperation} update={updateEquation}/>
     </div>
   );
 }
