f39e609490b3fcc6456a9d476e125a7e93178b50
shaolun <shaolun28@gmail.com>
2021-03-09 17:32:58 -0800

    all

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,11 +1,13 @@
 import React, {useState} from "react";
 import CalculatorFormSli11 from "./Components/CalculatorFormSli11/CalculatorFormSli11.js";
 
+import ResultDisplaySli11 from "./Components/ResultDisplaySli11/ResultDisplaySli11.js";
 
 
 function App() {
 
   const [equation, setEquation] = useState({});
+  const [result, setResult] = useState(0);
 
   function updateEquation(e){
     setEquation({
@@ -18,7 +20,8 @@ function App() {
   function doOperation(){
     switch(equation.operation){
       case "add":
-        console.log("Result: " + (parseFloat(equation.operand1) + parseFloat(equation.operand2)))
+        setResult(parseFloat(equation.operand1) + parseFloat(equation.operand2));
+        console.log("Result: " + result)
         break;
       default:
         console.log("Sorry we dont have that operation");
@@ -32,6 +35,7 @@ function App() {
     <div className="App">
       <h1>Shaolun's Calculator</h1>
       <CalculatorFormSli11 action={doOperation} update={updateEquation}/>
+      <ResultDisplaySli11 result={result}/>
     </div>
   );
 }
---
+++ calculator/src/Components/ResultDisplaySli11/ResultDisplaySli11.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function ResultDisplaySli11(props){
+    return (
+    <div className="col-2" key="result">{props.result}</div>
+    )
+}
+
+
+export default ResultDisplaySli11;
