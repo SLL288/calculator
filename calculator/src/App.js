8fd35cc5c2e54c00804361e5b0b7275922dc3f1a
shaolun <shaolun28@gmail.com>
2021-03-09 16:29:00 -0800

    added shaolun_interface to app:

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import CalculatorFormSli11 from "./Components/CalculatorFormSli11/CalculatorFormSli11.js";
 
+function update(){
+
+}
 
 function App() {
+
+  
+
   return (
     <div className="App">
       <h1>Shaolun's Calculator</h1>
+      <CalculatorFormSli11 update={}/>
     </div>
   );
 }
